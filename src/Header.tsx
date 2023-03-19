c8fb8528110ae0a4aabd546797863ad26a60d586
Jason Hargrove <rijaha@gmail.com>
2023-03-19 12:21:15 -0500

    my first commit

---
+++ src/Header.tsx
@@ -0,0 +1,14 @@
+export interface Props {
+  title: string
+  color?: string
+}
+
+const Header = (props: Props) => {
+  return (
+    <header>
+      <h1>{props.title}</h1>
+    </header>
+  )
+}
+
+// export default Header;
